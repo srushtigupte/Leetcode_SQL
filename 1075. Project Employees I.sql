cfea30632250b850db15904e64f45803384fe6e8
srushtigupte <119264916+srushtigupte@users.noreply.github.com>
2024-08-21 13:26:21 -0700

    Create 1075. Project Employees I.sql

---
+++ 1075. Project Employees I.sql
@@ -0,0 +1,4 @@
+SELECT p.project_id, ROUND(SUM(e.experience_years)/COUNT(p.employee_id), 2) AS average_years
+    FROM Project p LEFT JOIN Employee e
+    ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
